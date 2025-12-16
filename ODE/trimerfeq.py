fc3eb7db1c6db32b36d173cd1c501dcc0a2ac02f
silkyshin <77782287+silkyshin@users.noreply.github.com>
2025-12-16 16:10:48 +0000

    Update model name and kinetic constants for equilibrium

---
+++ ODE/trimerfeq.py
@@ -0,0 +1,17 @@
+# Model info
+model_name = "1-3-6 trimer based nucleation model with equilibrium constants"
+kinetic_constants = ['k1','K1','k2','K2']
+
+def ode_model(t, M, k):
+    M1, M3, M6 = M
+    k1, K1, k2, K2 = k
+
+    # Define reverse rates with equilibrium constant Kn
+    k_1 = k1 / K1
+    k_2 = k2 / K2
+  
+    dM1 = -3*k1*M1**3 + 3*k_1*M3
+    dM3 = k1*M1**3 - k_1*M3 - 2*k2*M3**2 + 2*k_2*M6
+    dM6 = 2*k3*M3**2 - k_3*M6
+
+    return [dM1,dM3,dM6]
