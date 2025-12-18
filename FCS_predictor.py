61c6302e4ae4a289568a7115ab115bf7d216ed71
silkyshin <77782287+silkyshin@users.noreply.github.com>
2025-12-18 12:25:29 +0000

    Update FCS_predictor.py

--- FCS_predictor.py
+++ FCS_predictor.py
@@ -2,35 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
-# --- 1. Parse command-line arguments ---
+# parse command line args
 parser = argparse.ArgumentParser(description="Predict FCS G0 and diffusion time from ODE solution")
 parser.add_argument('--csv', type=str, required=True, help="Path to ODE solution CSV")
-parser.add_argument('--sfactor', type=float, default=1.0, help="Scaling exponent v for tau_n = n^v (default 1.0)")
+parser.add_argument('--sfactor', type=float, default=0.5, help="Scaling exponent v for tau_n = n^v (default 0.5)")
 args = parser.parse_args()
 
 csv_file = args.csv
 v = args.sfactor
 
-# --- 2. Load ODE solution CSV ---
+# load ODE solutions - could try to make it more flexible
 data = np.loadtxt(csv_file, delimiter=',', skiprows=1)  # skip header
 t = data[:,0]
 M_t = data[:,1:]  # concentrations of oligomers
 
-# --- 3. Number of monomers per species ---
+# monomer per oligomer size
 n = np.arange(1, M_t.shape[1]+1)
 
-# --- 4. Normalize M_t to total monomer equivalents ---
+# M_t normalization
 total_monomers = np.sum(M_t * n, axis=1, keepdims=True)
 M_norm = (M_t * n) / total_monomers
 
-# --- 5. Predicted G0(t) ---
+# G0(t) prediction
 G0_pred = np.sum(M_norm**2, axis=1)
 
-# --- 6. Predicted diffusion time ---
+# tau prediction
 tau_n = n**v
 tauD_pred = np.sum(M_norm * tau_n, axis=1)
 
-# --- 7. Plot ---
+# plot - maybe do parse to change styles for ease of use?
 plt.figure(figsize=(8,6))
 plt.subplot(2,1,1)
 plt.plot(t, G0_pred, '-', label='Predicted G0')
@@ -46,7 +46,7 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-# --- 8. Save predictions ---
+# save
 save_file = csv_file.replace(".csv", "_FCS_pred.csv")
 header = "t,G0,tauD"
 np.savetxt(save_file, np.column_stack([t, G0_pred, tauD_pred]), delimiter=',', header=header, comments='', fmt='%.4g')
