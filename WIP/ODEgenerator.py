50d1ecea60743a1390a6cdd27ad666e3f8f8f18c
silkyshin <77782287+silkyshin@users.noreply.github.com>
2025-12-24 20:52:14 +0900

    Update ODEgenerator.py

--- WIP/ODEgenerator.py
+++ WIP/ODEgenerator.py
@@ -1,7 +1,15 @@
 import numpy as np
 import pandas as pd
-import
+import argparse
+import importlib.util
+import os
 
-# Generates ODEs according to the species and oligomerization model you input.
+# parse command line argument
+
+parser = argparse.ArgumentParser(description="Generates ODEs")
+parser.add_argument('--model', type=array required=True,
+                    help='name all oligomers by monomer size eg monomers dimers trimers 1,2,3')
+args = parser.parse_args()
+model_path = args.model
 
 # State the sizes of oligomers by monomer number
